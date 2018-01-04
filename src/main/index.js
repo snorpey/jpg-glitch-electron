cfcfd317902b40eb036cd50d7762ab5e020735fe
Georg Fischer <hi@snorpey.com>
2018-01-04 23:37:29 +0100

    update window title

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -16,12 +17,12 @@ function createWindow () {
 	/**
 	 * Initial window options
 	 */
-	mainWindow = new BrowserWindow( {
+
+	 mainWindow = new BrowserWindow( {
 		height: 563,
 		useContentSize: true,
 		width: 1000,
-		titleBarStyle: 'hidden',
-		title: app.getName()
+		titleBarStyle: 'hidden'
 	} );
 
 	mainWindow.loadURL( winURL );
