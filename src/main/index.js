e8be71081cc32ef9c82885a7a72ee14c8114ad2f
Georg Fischer <hi@snorpey.com>
2018-01-04 23:05:42 +0100

    show app name in window title

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,10 @@
 import { app, BrowserWindow } from 'electron';
-
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-	global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+	global.__static = require( 'path' ).join( __dirname, '/static' ).replace( /\\/g, '\\\\' )
 }
 
 let mainWindow
@@ -17,33 +16,34 @@ function createWindow () {
 	/**
 	 * Initial window options
 	 */
-	mainWindow = new BrowserWindow({
+	mainWindow = new BrowserWindow( {
 		height: 563,
 		useContentSize: true,
 		width: 1000,
-		titleBarStyle: 'hidden'
-	})
+		titleBarStyle: 'hidden',
+		title: app.getName()
+	} );
 
-	mainWindow.loadURL(winURL)
+	mainWindow.loadURL( winURL );
 
 	mainWindow.on('closed', () => {
 		mainWindow = null
-	})
+	} );
 }
 
-app.on('ready', createWindow)
+app.on( 'ready', createWindow );
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
+app.on( 'window-all-closed', () => {
+	if ( process.platform !== 'darwin' ) {
 		app.quit()
 	}
-})
+} );
 
-app.on('activate', () => {
-	if (mainWindow === null) {
+app.on( 'activate', () => {
+	if ( mainWindow === null ) {
 		createWindow()
 	}
-})
+} );
 
 /**
  * Auto Updater
