9b5a9d56972b4266f293e9425582fdf41b11dfa2
Georg Fischer <hi@snorpey.com>
2019-12-31 15:21:08 +0100

    add pwa

---
+++ precache-manifest.7ceceb50648fb3d027b713f9017fbb7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f5ef6f083b7a1d299986f4ba0eb2bf",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "b94071e79a3b8d6ffc0e",
+    "url": "/css/app.3347da17.css"
+  },
+  {
+    "revision": "07d8e39a964e0f7ac48b5aef998bb49f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b94071e79a3b8d6ffc0e",
+    "url": "/js/app.f83571e3.js"
+  },
+  {
+    "revision": "330f7bfb19b8164df15f",
+    "url": "/js/chunk-2d212edf.87944c56.js"
+  },
+  {
+    "revision": "41b36e81244f192eaa2c",
+    "url": "/js/chunk-2f3394ca.b8832f58.js"
+  },
+  {
+    "revision": "625042b59af00dc8898b",
+    "url": "/js/chunk-853b758e.964020e7.js"
+  },
+  {
+    "revision": "884c583a5e1f9fcee157",
+    "url": "/js/chunk-vendors.be3b1e6d.js"
+  },
+  {
+    "revision": "76e66f7d2fa19f56bc97fccff975569a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
